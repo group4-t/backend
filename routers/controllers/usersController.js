1c9faf40ed67333bd0a989b63c84a91c1614468a
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-14 09:46:07 +0300

    f

--- routers/controllers/usersController.js
+++ routers/controllers/usersController.js
@@ -30,24 +30,25 @@ const addNewUser = (req, res) => {
 };
 
 //add To User Fav
-const addToUserFav = (req, res) => {
+const addToUserFav = (req, res) => { //ماراح ادخل هنا الا ومعي بالفرونت يوزر اي دي او بقوله بالفرونت سجل اذا مافيه اي دي 
   const userId = req.params.id;
   const favitemId = req.params.trackId;
   const user = users.filter((ele) => {
     return ele.id == userId;
   });
+  if (user.Fav.length){
+    console.log(user.Fav.length );
   user.Fav.map((ele) => {
     if (ele.id == favitemId) {
       console.log("item already in fav");
     } else {
-      user.Fav.push(req.body);
-      // users.Fav.push({ id: favitemId }); //push all obj
+      user.Fav.push(req.body); // how can i push all fav obj?
     }
   });
-
+  
   addToFileFunction(users);
   res.json(users);
-};
+};}
 
 module.exports = {
   getAllUsers,
